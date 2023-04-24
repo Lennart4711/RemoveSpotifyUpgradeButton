0162dc9dc2009e8c4c7b4c99fc1c80259cb00d18
unknown <lennart.randomthings@gmail.com>
2023-04-24 13:33:13 +0200

    Added support for all languages

--- RemoveSpotifyUpgradeButton/content.js
+++ RemoveSpotifyUpgradeButton/content.js
@@ -1,5 +1,7 @@
 // Create a new mutation observer
 const observer = new MutationObserver(function (mutationsList, observer) {
+    // Search via class name so that the script will work in other languages
+    const upgradeButtonClasses = ["Button-sc-y0gtbx-0", "iVrvaH", "Upqw01TOXETOmR5Td7Dj"]
     // Check each mutation that occurred
     for (let mutation of mutationsList) {
         // Check if the mutation added any nodes
@@ -8,8 +10,15 @@ const observer = new MutationObserver(function (mutationsList, observer) {
             const buttons = document.getElementsByTagName('button');
             for (let button of buttons) {
                 if (button.textContent.trim() === 'Upgrade') {
-                    console.log('Buttons removed');
+                    // Cant delete the button directly because it will cause an error
                     button.style.display = 'none';
+                }
+                // if any of the upgrade button classes are in the button's class list, hide it
+                for (let upgradeButtonClass of upgradeButtonClasses) {
+                    if (button.classList.contains(upgradeButtonClass)) {
+                        button.style.display = 'none';
+                        break;
+                    }
                 }
             }
         }
